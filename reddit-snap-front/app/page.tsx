e2ea43cedeb7625b4ea735835309e7bab9b82d63
Yu Chiawei <andyiao19960607@gmail.com>
2024-09-21 21:32:58 +1000

    Use observable for the line chart

--- reddit-snap-front/app/page.tsx
+++ reddit-snap-front/app/page.tsx
@@ -1,4 +1,5 @@
 import LineChart from '@/app/ui/LineChart'
+import LineChart2 from '@/app/ui/LineChart_observable'
 import { fetchData } from './actions/dev_utility';
 import SearchForm from './ui/Search';
 
@@ -18,7 +19,8 @@ export default async function Page(
     <> 
       <div className='flex-col justify-items-center mt-64'>
         
-        <LineChart data={data}/> 
+        {/* <LineChart data={data}/> */}
+        <LineChart2 data={data}/> 
         
         <div className='flex justify-center'>
         
