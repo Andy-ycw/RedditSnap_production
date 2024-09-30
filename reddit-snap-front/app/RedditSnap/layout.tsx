27e84a1030db636b1b488e2dca559df6651d23f4
Yu Chiawei <andyiao19960607@gmail.com>
2024-09-30 13:45:19 +1000

    Update web metadata

--- reddit-snap-front/app/RedditSnap/layout.tsx
+++ reddit-snap-front/app/RedditSnap/layout.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'RedditSnap'
+}
+
 export default function RedditSnapLayout({ children }: { children: React.ReactNode }) {
     return (
         <>
